baaa77c8a1748eedaa6e8f3ff2c470a63f9c82a4
Alexandre Demeure <alexandre.demeure@univ-grenoble-alpes.fr>
2019-04-11 16:20:00 +0200

    Itération 0 sur la partie serveur

--- serveur/src/main/java/l3m/ClientAuthentificationServlet.java
+++ serveur/src/main/java/l3m/ClientAuthentificationServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+// Je suis passé par l'itération 0 du serveur...
 public class ClientAuthentificationServlet extends HttpServlet  {
 	private static final long serialVersionUID = 1L;
 
