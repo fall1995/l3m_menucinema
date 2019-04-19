9bd1c69e1887378617b79a9d317c227fc4036e81
idriss2017 <sagara.idriss@gmail.com>
2019-04-19 18:15:18 +0200

    test firebase ok

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,15 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {TmdbService} from './tmdb.service';
 import {HttpClientModule} from '@angular/common/http';
+import {AngularFireModule} from '@angular/fire';
+import {environment} from '../environments/environment';
+import { LoginComponent } from './login/login.component';
+import {AngularFireAuthModule} from '@angular/fire/auth';
+import {AngularFireDatabaseModule} from '@angular/fire/database';
+import {AngularFirestoreModule} from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+      AngularFireAuthModule,
+      AngularFireDatabaseModule,
+      AngularFirestoreModule,
+      AngularFireModule.initializeApp(environment.firebase)
   ],
   providers: [TmdbService],
   bootstrap: [AppComponent]
