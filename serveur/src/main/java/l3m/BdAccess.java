edb22ae61204c04607e97135f76968d5bbba888e
idriss2017 <sagara.idriss@gmail.com>
2019-04-19 20:09:02 +0200

    add serveur

---
+++ serveur/src/main/java/l3m/BdAccess.java
@@ -0,0 +1,12 @@
+package l3m;
+
+import java.sql.SQLException;
+
+// Je suis passé par l'itération 0 de la BD...
+public class BdAccess {
+
+	static String authentifyUser(String userId) throws SQLException {
+		return "TODO";
+	}
+
+}
