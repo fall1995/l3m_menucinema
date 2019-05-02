a5799a1c6dfec4e7cdcaba731d394e634aa7c01c
idriss2017 <sagara.idriss@gmail.com>
2019-05-03 01:06:35 +0200

    ajustement du menu et changement des routing du menu

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -8,13 +8,17 @@ import {SingleMovieComponent} from './single-movie/single-movie.component';
 import {AuthGuardService} from './service/auth-guard.service';
 import {UserProfilComponent} from './user-profil/user-profil.component';
 import {HomeComponent} from './home/home.component';
+import {CommandeComponent} from './commande/commande.component';
+import {PanierComponent} from './panier/panier.component';
 
 const routes: Routes = [
     {path: 'authentification/signin', component: SigninComponent},
     {path: 'authentification/signup', component: SignupComponent},
     {path: 'films', canActivate: [AuthGuardService], component: FilmsComponent},
     {path: 'menus', canActivate: [AuthGuardService], component: MenusComponent},
-    {path: 'profil', component: UserProfilComponent },
+    {path: 'user/dashbord', component: UserProfilComponent },
+    {path: 'user/commande', component: CommandeComponent },
+    {path: 'user/panier', component: PanierComponent },
     {path: '', component: HomeComponent },
     {path: 'films/view/:id', component: SingleMovieComponent},
     {path: '', redirectTo: 'films', pathMatch: 'full'},
