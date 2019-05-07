a1756dee22045b1e0ee6916a26711f860543f366
idriss2017 <sagara.idriss@gmail.com>
2019-05-07 12:34:36 +0200

    maintenant on veut qu'il se connect au moment de validation de commande

--- client/src/app/panier/panier.component.ts
+++ client/src/app/panier/panier.component.ts
@@ -4,6 +4,7 @@ import {ListePlats} from '../menu-commade-data/Menu';
 import {Router} from '@angular/router';
 import {CommandeService} from '../service/commande.service';
 import {Commande} from '../menu-commade-data/commande';
+import {AngularFireAuth} from '@angular/fire/auth';
 
 @Component({
     selector: 'app-panier',
@@ -13,15 +14,26 @@ import {Commande} from '../menu-commade-data/commande';
 export class PanierComponent implements OnInit {
 
     constructor(private storageService: StorageService, private route: Router,
-                private commandeService: CommandeService) {
+                private commandeService: CommandeService, private afAuth: AngularFireAuth) {
     }
 
     panier: any[]; // variable qui stocke le tableaux de plat
     movie: any[]; // variable qui stock les films selectionnées
-    commande: any;
+    //commande: any;
+    isAuth: boolean; // boolean indiquant s'il est connecté
 
     ngOnInit() {
         this.init();
+        this.afAuth.auth.onAuthStateChanged(
+            (user) => {
+                if (user) {
+                    this.isAuth = true;
+
+                } else {
+                    this.isAuth = false;
+                }
+            }
+        );
     }
 
     init() {
@@ -57,16 +69,20 @@ export class PanierComponent implements OnInit {
         let idFilm = localStorage.getItem('movieId');
         console.log('contenu de idPlat' + idFilm);
         let adresse = localStorage.getItem('adresse');
-        this.commandeService.sendCmd({
-            // variable que le serveur s'attend a recevoir
-            idClient: idClient,
-            idPlats: idPlat,
-            idFilms: idFilm,
-            adresseLivraison: adresse,
-        }).then(data => {
-            console.log('envoie après verification au serveur ok');
-        });
-        console.log('verification commande pas ok');
+        if (this.isAuth){
+            this.commandeService.sendCmd({
+                // variable que le serveur s'attend a recevoir
+                idClient: idClient,
+                idPlats: idPlat,
+                idFilms: idFilm,
+                adresseLivraison: adresse,
+            }).then(data => {
+                console.log('envoie après verification au serveur ok');
+            });
+        } else {
+            this.route.navigate(['/authentification/signin']);
+        }
+
     }
 
     /**
