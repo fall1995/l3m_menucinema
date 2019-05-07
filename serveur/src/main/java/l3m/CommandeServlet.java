32f1dea8ae193d04da3b6d8b40398e09d31c58bf
idriss2017 <sagara.idriss@gmail.com>
2019-05-07 16:41:07 +0200

    commande dopost

--- serveur/src/main/java/l3m/CommandeServlet.java
+++ serveur/src/main/java/l3m/CommandeServlet.java
@@ -18,72 +18,69 @@ import javax.servlet.http.HttpServletResponse;
 
 /**
  * @author Groupe6 CommandeServlet est une classe qui permet de modeliser les
- commandes d'un client
+ * commandes d'un client
  */
 public class CommandeServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+
         response.setContentType("application/json");
         String idCommande = request.getParameter("idCommande");
         Commande commande;
-        
+
         try {
             commande = GestionnaireCommande.getCommande(idCommande);
-            
+
             response.setContentType("application/json");
             response.setStatus(HttpServletResponse.SC_OK);
-            response.getWriter().println( new Gson().toJson(commande) );
-            
+            response.getWriter().println(new Gson().toJson(commande));
+
         } catch (SQLException ex) {
             Logger.getLogger(CommandeServlet.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
-    }
 
+    }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+
         Commande commande = new Commande();
-        commande.setIdClient( request.getParameter("idClient") );
-        commande.setAdresseLivraison(request.getParameter("adresseLivraison") );
-        
-        String buffer = (request.getParameter("idPlats")).toString();
-        buffer = buffer.replaceAll("[", "");
+        commande.setIdClient(request.getParameter("idClient"));
+        commande.setAdresseLivraison(request.getParameter("adresseLivraison"));
+
+        String buffer = "";
+        buffer = request.getParameter("idPlats");
+        buffer = buffer.replaceAll("[\\[\\]]", "");
         String[] plats = buffer.split(",");
-        
-        System.out.println((request.getParameter("idPlats")).toString());
-        for ( int i = 0 ; i < plats.length ; i ++ ){
+
+        for (int i = 0; i < plats.length; i++) {
             commande.getIdPlats().add(plats[i]);
         }
-        
-        buffer = (request.getParameter("idFilms")).toString();
-        buffer = buffer.replaceAll("[", "");
+        buffer = request.getParameter("idFilms");
+        buffer = buffer.replaceAll("[\\[\\]]", "");
         String[] films = buffer.split(",");
-        
-        for ( int i = 0 ; i < films.length ; i ++ ){
+        for (int i = 0; i < films.length; i++) {
             commande.getIdFilms().add(films[i]);
         }
-        
+
         GestionnaireCommande gc;
-        
+
         try {
-            gc = new GestionnaireCommande(  commande.getIdClient(),
-                                            commande.getIdPlats(),
-                                            commande.getIdFilms(),
-                                            commande.getAdresseLivraison()
-                                         );
-            
+            gc = new GestionnaireCommande(commande.getIdClient(),
+                    commande.getIdPlats(),
+                    commande.getIdFilms(),
+                    commande.getAdresseLivraison()
+            );
+
             gc.enregistrerCommandeDB();
-            
+
             response.setContentType("application/json");
             response.setStatus(HttpServletResponse.SC_OK);
-            response.getWriter().println( gc.CommandeToJson() );
-            
+            response.getWriter().println(gc.CommandeToJson());
+
         } catch (SQLException ex) {
             Logger.getLogger(CommandeServlet.class.getName()).log(Level.SEVERE, null, ex);
         } catch (Exception ex) {
@@ -91,28 +88,25 @@ public class CommandeServlet extends HttpServlet {
         }
     }
 
-
-
     @Override
     protected void doPut(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-    
+
         Enumeration<String> P = request.getParameterNames();
         HashMap<String, String> parametres = new HashMap();
 
-
     }
-      
 
     @Override
     protected void doDelete(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         String idClient = request.getParameter("idCommande");
-        
+
     }
 
     /**
      * Methode qui permet de convertir un tableau de string en liste
+     *
      * @param Tableau
      */
     static <T> List<T> toArrayList(T[] Tableau) {
